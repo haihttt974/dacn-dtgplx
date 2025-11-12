5ac74fa946cab0bc3affd7a87636bedabc34eb85
haiit <leduyhai090704@gmail.com>
2025-11-12 19:39:11 +0700

    !!!

---
+++ dacn-dtgplx/Controllers/AdminUsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using dacn_dtgplx.Models;
+
+namespace dacn_dtgplx.Controllers
+{
+    [Route("admin/users")]
+    public class AdminUsersController : Controller
+    {
+        private readonly DtGplxContext _context;
+
+        public AdminUsersController(DtGplxContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index(int? roleId)
+        {
+            var users = _context.Users.Include(u => u.Role).AsQueryable();
+            if (roleId.HasValue && roleId.Value > 0)
+                users = users.Where(u => u.RoleId == roleId);
+
+            ViewBag.Roles = await _context.Roles.ToListAsync();
+            ViewBag.CurrentRoleId = roleId; // <-- truyền ra View
+
+            return View(await users.ToListAsync());
+        }
+    }
+}
