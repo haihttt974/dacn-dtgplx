57bc1d41d7c37793d44db1fd63e0d5fa08bc30ea
haiit <leduyhai090704@gmail.com>
2025-12-01 01:34:39 +0700

    Controller + View (Home)

--- dacn-dtgplx/Controllers/HomeController.cs
+++ dacn-dtgplx/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
-using System.Diagnostics;
-using dacn_dtgplx.Models;
+﻿using dacn_dtgplx.Models;
+using dacn_dtgplx.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace dacn_dtgplx.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DtGplxContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DtGplxContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var vm = new HomeViewModel
+            {
+                Courses = await _context.KhoaHocs
+                    .OrderBy(k => k.NgayBatDau)
+                    .ToListAsync(),
+
+                Instructors = await _context.TtGiaoViens
+                    .Include(g => g.User) // nếu cần
+                    .ToListAsync(),
+
+                Testimonials = await _context.PhanHois
+                    .Include(p => p.User)
+                    .OrderByDescending(p => p.ThoiGianPh)
+                    .Take(6)
+                    .ToListAsync(),
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
