3b269d470adb90c4856491be0426932ba9f4c762
haiit <leduyhai090704@gmail.com>
2025-12-17 22:49:57 +0700

    update report PRO

--- dacn-dtgplx/ViewModels/Reports/HangValueVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace dacn_dtgplx.ViewModels.Reports
-{
-    public class HangValueVM
-    {
-        public string Ten { get; set; } = "";
-        public decimal Value { get; set; }
-    }
-}
